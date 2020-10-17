149706d596090fc90ca2103aeb313459a1134a12
heissonwillen <heisson70@gmail.com>
2020-10-17 12:45:52 -0300

    Adiciona questão sobre laços de repetição

---
+++ monitoria/10-17/repeticao.py
@@ -0,0 +1,20 @@
+# a = 10
+
+# while a == 10:
+# 	a = int(input())
+
+
+# variavel = 100
+
+# while True:
+# 	print("Ainda estamos no loop")
+# 	a = int(input())
+
+# 	variavel = variavel - a
+
+# 	if variavel > 0:
+# 		break
+
+# print("Saimos do loop")
+
+
