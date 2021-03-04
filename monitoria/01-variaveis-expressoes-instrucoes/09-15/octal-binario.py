9e4560c9ccb1c232f75182d3302580430636f5f0
heissonwillen <heisson70@gmail.com>
2021-03-04 10:33:44 -0300

    Remoção de arquivos desnecessários

--- monitoria/01-variaveis-expressoes-instrucoes/09-15/octal-binario.py
+++
@@ -1,6 +0,0 @@
-num = 261
-
-def octal_to_binary(octal):
-    octal = str(octal)
-    binary = ''
-    for character in octal:
--- monitoria/01-variaveis-expressoes-instrucoes/09-15/uri-3055.py
+++
@@ -1,9 +0,0 @@
-def nota(a, m):
-    return 2*m-a
-
-# nota = lambda a, m: 2*m-a
-
-a = int(input())
-m = int(input())
-
-print(f'{nota(a, m)}')
