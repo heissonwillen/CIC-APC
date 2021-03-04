9fa48d483011ed4b09979fd0d037038023c2a4cb
heissonwillen <heisson70@gmail.com>
2021-03-04 09:53:05 -0300

    Adiciona aula do dia 4 de março

---
+++ monitoria/01-variaveis-expressoes-instrucoes/problemas/entrada.py
@@ -0,0 +1,15 @@
+# a, b, c, d, e
+# 1 2 3 4 5
+
+# a, b, c = map(float, input().split())
+
+# print(type(a))
+# print(c)
+
+"""
+MM/AAAA
+"""
+
+mes, ano = map(int, input().split("/"))
+
+print(type(mes), type(ano))
---
+++ monitoria/01-variaveis-expressoes-instrucoes/problemas/uri-3055.py
@@ -0,0 +1,11 @@
+"""
+media = (a + b)/2
+b = 2*media - a
+"""
+
+a = int(input())
+media = int(input())
+
+b = 2*media - a
+
+print(f"{b}")
